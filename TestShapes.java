5c40e6df6b4e555eddd799289c2060b53403ac07
Oloow22 <113370402+Oloow22@users.noreply.github.com>
2023-03-30 13:54:12 +0300

    Update TestShapes.java
    Added the rectangle shape

--- TestShapes.java
+++ TestShapes.java
@@ -31,3 +31,49 @@ public class TestShapes {
         System.out.println("Circle contains point (1,1): " + circle.contains(point));
     }
 }
+
+// rectangle shape
+class Rectangle extends Shape {
+    double width;
+    double height;
+
+    public Rectangle(Post center, double width, double height){
+        super(center); // this allows us to use the centre method from the shape class
+        this.width = width;
+        this.height = height;
+    }
+
+    // getters 
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    // perimeter and area
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    // getting the centre
+    @Override
+    public boolean contains(Post pos) {
+        double halfWidth = width / 2;
+        double halfHeight = height / 2;
+        return pos.getX() >= center.getX() - halfWidth
+                && pos.getX() <= center.getX() + halfWidth
+                && pos.getY() >= center.getY() - halfHeight
+                && pos.getY() <= center.getY() + halfHeight;
+    }
+
+
+
+}
