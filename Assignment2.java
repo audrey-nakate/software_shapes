b00e56a3a8f6316bb205c3de9066148bab2cf88a
Your Name <you@example.com>
2023-04-27 22:20:44 +0300

    square

--- Assignment2.java
+++ Assignment2.java
@@ -95,31 +95,4 @@ class Rectangle extends Shape {
 
 }
 
-class Square extends Shape {
-    double size;
 
-    public Square(Point center, double size) {
-        super(center);
-        this.size = size;
-        this.name = "square";
-    }
-
-    public double getSize() {
-        return size;
-    }
-
-    public double perimeter() {
-        return 4 * size;
-    }
-
-    public double area() {
-        return size * size;
-    }
-
-    public boolean contains(Point posn) {
-        double halfSize = size / 2;
-        return posn.x >= center.x - halfSize && posn.x <= center.x + halfSize && posn.y >= center.y - halfSize
-                && posn.y <= center.y + halfSize;
-    }
-
-}
